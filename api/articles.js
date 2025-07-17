a4c19ad2026990c177a3f9987330c70343a99878
ThooL <thools1e2@gmail.com>
2025-07-17 21:13:32 +0530

    fix: export default for Vercel API

--- api/articles.js
+++ api/articles.js
@@ -1,8 +1,12 @@
-module.exports = (req, res) => {
+export default function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   const articles = [
     { id: 1, title: 'Cricket World Cup 2025 Begins', content: 'Opening match in Ahmedabad...' },
     { id: 2, title: 'ISL Final 2025', content: 'Kerala Blasters win in penalty shootout...' }
   ];
+
   res.status(200).json(articles);
-};
+}
