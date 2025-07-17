dadfd08a8f331a4cfc0fd4ad374e4bf5647badc1
ThooL <thools1e2@gmail.com>
2025-07-17 21:04:26 +0530

    add module

--- api/index.js
+++ api/index.js
@@ -8,6 +8,9 @@ const articles = [
   { id: 2, title: 'ISL Final 2025', content: 'Kerala Blasters win in penalty shootout...' }
 ];
 
-module.exports = app.get('/api/articles', (req, res) => {
+app.get('/api/articles', (req, res) => {
   res.json(articles);
 });
+
+// 👇 Export the whole Express app
+module.exports = app;
